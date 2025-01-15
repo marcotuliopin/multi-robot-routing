9a479cecc4424062e1c0b3d2df6f25b57a56fc7a
Marco Tulio <marcotuliopin@gmail.com>
2025-01-15 13:46:11 -0300

    feat: create better tests

--- test.py
+++ test.py
@@ -23,6 +23,7 @@ args = {
     "num_agents": [2, 3, 6, 8, 10, 12, 14, 16, 18, 20],
 }
 
+
 def read_map(m):
     with open(m, "r") as f:
         lines = f.readlines()
@@ -35,33 +36,47 @@ def read_map(m):
         rvalues = np.array([float(line) for line in lines[num_rewards + 1 :]])
     return num_rewards, dispersion, rpositions, rvalues
 
+
 # Analisar o aumento do tempo de execução conforme o número de recompensas aumenta. Para isso, executar cada mapa
-# 10 vezes para 3 agentes e calcular a média e o desvio padrão do tempo de execução.
+# 5 vezes para 3 agentes e calcular a média e o desvio padrão do tempo de execução.
 
 map_exec_time = {}
 
 for m in args["maps"]:
+    print(f"Executing map {m}...")
+
     num_rewards, dispersion, rpositions, rvalues = read_map(m)
     budget = dispersion * 20
 
-    start = time.time()
-    paths = movns(
-        num_rewards,
-        rpositions,
-        rvalues,
-        budget,
-        seed=42,
-        num_agents=3,
-    )
-    end = time.time()
-    duration = end - start
-    map_exec_time[num_rewards] = duration
+    exec_time = []
+    for i in range(5):
+        start = time.time()
+        paths = movns(
+            num_rewards,
+            rpositions,
+            rvalues,
+            budget,
+            seed=42,
+            num_agents=3,
+        )
+        end = time.time()
+        duration = end - start
+        exec_time.append(duration)
 
-with open('tests/map_exec_time.pkl', 'wb') as f:
+    exec_time_avg = np.mean(exec_time)
+    exec_time_std = np.std(exec_time)
+    map_exec_time[num_rewards] = {
+        "avg": exec_time_avg,
+        "std": exec_time_std,
+    }
+
+with open("tests/map_exec_time.pkl", "wb") as f:
     pickle.dump(map_exec_time, f)
-    
+
 # Analisar o aumento do tempo de execução conforme o número de agentes aumenta. Para isso, executar o mapa de 100 recompensas
 # 10 vezes para cada número de agentes e calcular a média e o desvio padrão do tempo de execução.
+
+# Analisar o aumento do tempo de execução conforme o budget aumenta.
 
 # Analisar o número de vizinhos gerados conforme o número de agentes aumenta. Para isso, executar o mapa de 100 recompensas
 # 10 vezes para cada número de agentes e calcular a média e o desvio padrão do número de vizinhos gerados em cada iteração.
