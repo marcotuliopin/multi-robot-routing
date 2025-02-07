74990c14fd6cba9940eca9b35cff4448cfdef9ad
Marco Tulio <marcotuliopin@gmail.com>
2025-02-07 09:24:44 -0300

    [op-testing] - Fix map generation

--- generate_rewards.py
+++ generate_rewards.py
@@ -2,11 +2,21 @@ import numpy as np
 
 
 def generate_dispersed_rewards(num_rewards, dispersion, output_file):
-    # Gerar posições de recompensas dispersas
-    rpositions = np.random.randint(0, dispersion, size=(num_rewards, 2))
+    # Gerar a primeira posição de recompensa aleatoriamente
+    rpositions = [np.random.randint(0, dispersion, size=2)]
+
+    # Gerar posições de recompensas subsequentes
+    for _ in range(1, num_rewards):
+        while True:
+            new_pos = rpositions[-1] + np.random.randint(-dispersion, dispersion + 1, size=2)
+            if np.all(new_pos >= 0) and np.all(new_pos < dispersion * num_rewards):
+                rpositions.append(new_pos)
+                break
+
+    rpositions = np.array(rpositions)
 
     # Gerar valores de recompensas
-    rvalues = np.arange(0, num_rewards * 5, 5)
+    rvalues = np.random.randint(1, num_rewards * 2, size=num_rewards)
 
     # Salvar as recompensas em um arquivo
     with open(output_file, "w") as f:
@@ -18,44 +28,45 @@ def generate_dispersed_rewards(num_rewards, dispersion, output_file):
 
 
 if __name__ == "__main__":
-    args = [
-        {
-            "num_rewards": 10,
-            "dispersion": 70.0,
-            "output_file": "maps/dispersed_small.txt",
-        },
-        {
-            "num_rewards": 30,
-            "dispersion": 70.0,
-            "output_file": "maps/dispersed_large.txt",
-        },
-        {
-            "num_rewards": 50,
-            "dispersion": 70.0,
-            "output_file": "maps/dispersed_huge.txt",
-        },
-        {
-            "num_rewards": 100,
-            "dispersion": 70.0,
-            "output_file": "maps/dispersed_gigantic.txt",
-        },
-        {
-            "num_rewards": 200,
-            "dispersion": 70.0,
-            "output_file": "maps/dispersed_colossal.txt",
-        },
-        {
-            "num_rewards": 500,
-            "dispersion": 70.0,
-            "output_file": "maps/dispersed_titanic.txt",
-        },
-        {
-            "num_rewards": 1000,
-            "dispersion": 70.0,
-            "output_file": "maps/dispersed_humongous.txt",
-        },
-    ]
-    for arg in args:
-        generate_dispersed_rewards(
-            arg["num_rewards"], arg["dispersion"], arg["output_file"]
-        )
+    # args = [
+    #     {
+    #         "num_rewards": 10,
+    #         "dispersion": 70.0,
+    #         "output_file": "maps/dispersed_small.txt",
+    #     },
+    #     {
+    #         "num_rewards": 30,
+    #         "dispersion": 70.0,
+    #         "output_file": "maps/dispersed_large.txt",
+    #     },
+    #     {
+    #         "num_rewards": 50,
+    #         "dispersion": 70.0,
+    #         "output_file": "maps/dispersed_huge.txt",
+    #     },
+    #     {
+    #         "num_rewards": 100,
+    #         "dispersion": 70.0,
+    #         "output_file": "maps/dispersed_gigantic.txt",
+    #     },
+    #     {
+    #         "num_rewards": 200,
+    #         "dispersion": 70.0,
+    #         "output_file": "maps/dispersed_colossal.txt",
+    #     },
+    #     {
+    #         "num_rewards": 500,
+    #         "dispersion": 70.0,
+    #         "output_file": "maps/dispersed_titanic.txt",
+    #     },
+    #     {
+    #         "num_rewards": 1000,
+    #         "dispersion": 70.0,
+    #         "output_file": "maps/dispersed_humongous.txt",
+    #     },
+    # ]
+    # for arg in args:
+    #     generate_dispersed_rewards(
+    #         arg["num_rewards"], arg["dispersion"], arg["output_file"]
+    #     )
+    generate_dispersed_rewards(100, 6, "maps/dispersed_near.txt")
